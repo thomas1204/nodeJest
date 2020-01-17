419f5a44e56bda5f1f20735ef810cdb85dcdaf37
shayamthomas <shayamthomas.official@gmail.com>
2020-01-17 22:43:49 +0530

    mongoose added

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,6 +1,17 @@
 const TodoController = require('../../controllers/todo.controller');
+const TotoModel = require('../../model/todo.model');
 
+TotoModel.create = jest.fn();
 
 describe("TodoController.createTodo", () => {
-
+	
+	it("should have a create todo function", () => {
+		expect(typeof TodoController.createTodo).toBe("function");
+	});
+	
+	it('should call TodoModel.create', () => {
+		TodoController.createTodo();
+		expect(TotoModel.create).toBeCalled();
+	});
+	
 });
